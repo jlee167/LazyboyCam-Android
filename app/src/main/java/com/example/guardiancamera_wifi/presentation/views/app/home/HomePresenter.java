ca38e44d511573128e339acb80f8823fdc5e83ac
Jihoon Lee <anfwkrpdnjs179@gmail.com>
2021-09-01 03:37:40 +0900

    Skeleton presenter for home fragment

---
+++ app/src/main/java/com/example/guardiancamera_wifi/presentation/views/app/home/HomePresenter.java
@@ -0,0 +1,35 @@
+package com.example.guardiancamera_wifi.presentation.views.app.home;
+
+import com.example.guardiancamera_wifi.domain.broadcasts.ServiceMsgBroadcast;
+
+public class HomePresenter {
+
+    HomePresenter() {
+
+    }
+
+    public void handleCameraConnection() {
+
+    }
+
+    public void handleCameraDisconnection() {
+
+    }
+
+    public void handleStreamStart() {
+
+    }
+
+    public void handleStreamStop() {
+
+    }
+
+    public void handleEmergencyStart() {
+
+    }
+
+    public void handleEmergencyStop() {
+
+    }
+
+}
