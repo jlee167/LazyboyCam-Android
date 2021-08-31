efc350137204f47d8fc7b4807e2c8a4d390f3919
Jihoon Lee <anfwkrpdnjs179@gmail.com>
2021-09-01 03:17:32 +0900

    Fix: Fix confusing wording for camera activation.

--- app/src/main/java/com/example/guardiancamera_wifi/data/configs/WifiCameraProtocol.java
+++ app/src/main/java/com/example/guardiancamera_wifi/data/configs/WifiCameraProtocol.java
@@ -6,7 +6,7 @@ public class WifiCameraProtocol {
     public static final byte CAM_CMD_START_EMERGENCY = 0x10;
     public static final byte CAM_CMD_STOP_EMERGENCY = 0x11;
     public static final byte CAM_CMD_DISCONNECT = 0x15;
-    public static final byte CAM_CMD_REGISTER = 0x12;
+    public static final byte CAM_CMD_ACTIVATE = 0x12;
     public static final byte CAM_RESP_ACK = 0x41;
     public static final byte CAM_RESP_ERR = 0x42;
 }
