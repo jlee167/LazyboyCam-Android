3c0e20da5082c474f14c44c13d44223f59849172
Jihoon Lee <anfwkrpdnjs179@gmail.com>
2021-09-01 03:21:35 +0900

    Feat: Usecase usage defined. All usecases will perform action with 'execute' function. Parameters and Results will be parametrized into template. Errors will be thrown in exception rather than error code. Return values of execute() will be for data, not flags and errors.

--- app/src/main/java/com/example/guardiancamera_wifi/domain/usecases/base/UseCase.java
+++ app/src/main/java/com/example/guardiancamera_wifi/domain/usecases/base/UseCase.java
@@ -1,6 +1,13 @@
 package com.example.guardiancamera_wifi.domain.usecases.base;
 
-public interface UseCase<T> {
-    //@Todo
-    //public T execute();
+import com.example.guardiancamera_wifi.domain.usecases.login.exceptions.InvalidCredentialException;
+import com.example.guardiancamera_wifi.domain.usecases.userProfile.exceptions.UserNotFoundException;
+
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+
+public interface UseCase<Result, Params> {
+    Result execute(Params params) throws IOException, UserNotFoundException, ExecutionException, InterruptedException, JSONException, InvalidCredentialException;
 }
