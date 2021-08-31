dd270c886f4b3725a3e3db73b800648de002b531
Jihoon Lee <anfwkrpdnjs179@gmail.com>
2021-09-01 03:52:14 +0900

    Feat: Use Presenter for main menu fragment. Refactoring: Break case statement for broadcast receiver into smaller functions

--- app/src/main/java/com/example/guardiancamera_wifi/presentation/views/app/MainMenuActivity.java
+++ app/src/main/java/com/example/guardiancamera_wifi/presentation/views/app/MainMenuActivity.java
@@ -1,204 +1,115 @@
 package com.example.guardiancamera_wifi.presentation.views.app;
 
-import android.Manifest;
-import android.annotation.SuppressLint;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
 import android.content.IntentFilter;
-import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.example.guardiancamera_wifi.R;
 import com.example.guardiancamera_wifi.domain.broadcasts.EmergencyBroadcast;
+import com.example.guardiancamera_wifi.domain.models.ClientStreamData;
 import com.example.guardiancamera_wifi.domain.models.VideoConfig;
-import com.example.guardiancamera_wifi.domain.services.EmergencyService;
-import com.example.guardiancamera_wifi.domain.services.GeolocationService;
 import com.example.guardiancamera_wifi.presentation.views.app.home.HomeFragment;
 import com.example.guardiancamera_wifi.presentation.views.app.peerList.PeerListFragment;
 import com.example.guardiancamera_wifi.presentation.views.app.setting.SettingsFragment;
-import com.example.guardiancamera_wifi.presentation.views.app.watch.StreamFragment;
+import com.example.guardiancamera_wifi.presentation.views.app.watch.WatchStreamFragment;
 
 
 public class MainMenuActivity extends AppCompatActivity {
 
-    /* Control Buttons */
+    MainMenuPresenter presenter;
+
     TextView captureServiceBtn;
     TextView viewVideoBtn;
     TextView peerListBtn;
     TextView settingBtn;
     TextView homeBtn;
 
-    Intent emergencyIntent;
-    Intent geoLocationIntent;
 
-
-    /**
-     * Helper function for changing fragment
-     *
-     * @param newFragment Target Fragment
-     */
     private void changeFragment(Fragment newFragment) {
-        FrameLayout container = (FrameLayout) findViewById(R.id.contentsFrame);
+        FrameLayout container = findViewById(R.id.contentsFrame);
         container.removeAllViews();
-
         final FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         transaction.add(R.id.contentsFrame, newFragment);
         transaction.commit();
     }
 
+    public void onStreamStart(ClientStreamData clientStreamData) {
+        ((TextView) findViewById(R.id.streamStatusView)).setText("Active");
+        ((TextView) findViewById(R.id.systemLog)).append("Stream Started. \n");
+        ((TextView) findViewById(R.id.systemLog)).append(
+                "Video URL: " + clientStreamData.getVideoDestUrl() + "\n");
+        ((TextView) findViewById(R.id.systemLog)).append(
+                "Audio URL: " + clientStreamData.getAudioDestUrl() + "\n");
+        ((TextView) findViewById(R.id.systemLog)).append(
+                "Geo URL: " + clientStreamData.getGeoDestUrl() + "\n");
+    }
+
+    public void onCameraConnected() {
+        ((TextView) findViewById(R.id.cameraStatusView)).setText("Online");
+        ((TextView) findViewById(R.id.systemLog)).append(
+                "Camera Connected. Serial: " + VideoConfig.serialNumber + "\n");
+    }
+
+    public void onCameraDisconnected() {
+        ((TextView) findViewById(R.id.cameraStatusView)).setText("Offline");
+        ((TextView) findViewById(R.id.systemLog)).append(
+                "Camera Disconnected" + VideoConfig.serialNumber + "\n");
+    }
+
+    public void onEmergencyStart(ClientStreamData clientStreamData) {
+        ((TextView) findViewById(R.id.userStatusView)).setText("Danger");
+        ((TextView) findViewById(R.id.systemLog)).append(
+                "Emergency Protocol Started. \n");
+    }
+
+    public void onEmergencyStop() {
+        ((TextView) findViewById(R.id.userStatusView)).setText("Fine");
+        ((TextView) findViewById(R.id.systemLog)).append(
+                "Emergency is over. \n");
+    }
+
+    public void onStreamStop() {
+        ((TextView) findViewById(R.id.streamStatusView)).setText("Inctive");
+        ((TextView) findViewById(R.id.systemLog)).append("Stream Stopped. \n");
+    }
 
     /**
      * Create activity intents and initialize control buttons' UI.
      * Connect respective buttons to corresponding services and activities.
      */
-    private void initButtonsUI() {
-        IntentFilter filter = new IntentFilter();
-        filter.addAction("MainMenuActivity");
-        BroadcastReceiver serviceMsgReceiver = new BroadcastReceiver() {
-            @SuppressLint("SetTextI18n")
-            @Override
-            public void onReceive(Context context, Intent intent) {
-                String action = intent.getAction();
-                Bundle extras = intent.getExtras();
-
-                assert action != null;
-                switch (action) {
-                    case "stream.start":
-                        ((TextView)findViewById(R.id.streamStatus)).setText("Active");
-                        ((TextView)findViewById(R.id.streamLog)).append("Stream Started. \n");
-                        ((TextView)findViewById(R.id.streamLog)).append(
-                                "Video URL: " + intent.getStringExtra("videoDestUrl") + "\n");
-                        ((TextView)findViewById(R.id.streamLog)).append(
-                                "Audio URL: " + intent.getStringExtra("audioDestUrl") +"\n");
-                        ((TextView)findViewById(R.id.streamLog)).append(
-                                "Geo URL: " + intent.getStringExtra("geoDestUrl") +"\n");
-                        break;
-
-                    case "camera.connected":
-                        ((TextView)findViewById(R.id.cameraStatus)).setText("Online");
-                        ((TextView)findViewById(R.id.streamLog)).append(
-                                "Camera Connected. Serial: " + VideoConfig.serialNumber +"\n");
-                        break;
-
-                    case "camera.disconnected":
-                        ((TextView)findViewById(R.id.cameraStatus)).setText("Offline");
-                        ((TextView)findViewById(R.id.streamLog)).append(
-                                "Camera Disconnected" + VideoConfig.serialNumber +"\n");
-                        break;
-
-                    case "emergency.start":
-                        geoLocationIntent.putExtra("destUrl",
-                                intent.getStringExtra("geoDestUrl"));
-
-                        if (GeolocationService.isRunning()) {
-                            stopService(geoLocationIntent);
-                            while (GeolocationService.isRunning()) {};
-                        }
-                        startService(geoLocationIntent);
-
-                        ((TextView)findViewById(R.id.clientStatus)).setText("Danger");
-                        ((TextView)findViewById(R.id.streamLog)).append(
-                                "Emergency Protocol Started. \n");
-                        break;
-
-                    case "emergency.stop":
-                        if (GeolocationService.isRunning())
-                            stopService(geoLocationIntent);
-
-                        ((TextView)findViewById(R.id.clientStatus)).setText("Fine");
-                        ((TextView)findViewById(R.id.streamLog)).append(
-                                "Emergency is over. \n");
-                        break;
-
-                    case "stream.stop":
-                        ((TextView)findViewById(R.id.streamStatus)).setText("Inctive");
-                        ((TextView)findViewById(R.id.streamLog)).append("Stream Stopped. \n");
-
-                    default:
-                        break;
-                }
-            }
-        };
-        registerReceiver(serviceMsgReceiver, filter);
-
+    private void initUI() {
         captureServiceBtn = findViewById(R.id.captureStartBtn);
-        captureServiceBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (!EmergencyService.isRunning()) {
-                    startService(emergencyIntent);
-                    captureServiceBtn.setText(R.string.MENU_STOP_CAPTURE);
-                } else {
-                    stopService(emergencyIntent);
-                    captureServiceBtn.setText(R.string.MENU_START_CAPTURE);
-                }
+        viewVideoBtn = findViewById(R.id.videoViewBtn);
+        peerListBtn = findViewById(R.id.peerListBtn);
+        settingBtn = findViewById(R.id.settingBtn);
+        homeBtn = findViewById(R.id.homeBtn);
+
+        captureServiceBtn.setOnClickListener(v -> {
+            if (!presenter.isStreaming()) {
+                presenter.startStreamingService();
+                captureServiceBtn.setText(R.string.MENU_STOP_CAPTURE);
+            } else {
+                presenter.stopStreamingService();
+                captureServiceBtn.setText(R.string.MENU_START_CAPTURE);
             }
         });
 
-
-        viewVideoBtn = (TextView) findViewById(R.id.videoViewBtn);
-        viewVideoBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                changeFragment(new StreamFragment());
-            }
+        viewVideoBtn.setOnClickListener(v -> changeFragment(new WatchStreamFragment()));
+        peerListBtn.setOnClickListener(v -> changeFragment(new PeerListFragment()));
+        settingBtn.setOnClickListener(v -> changeFragment(new SettingsFragment()));
+        homeBtn.setOnClickListener(v -> {
+            Fragment homeFragment = new HomeFragment();
+            changeFragment(homeFragment);
         });
-
-
-        peerListBtn = (TextView) findViewById(R.id.peerListBtn);
-        peerListBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                changeFragment(new PeerListFragment());
-            }
-        });
-
-        settingBtn = (TextView) findViewById(R.id.settingBtn);
-        settingBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                changeFragment(new SettingsFragment());
-            }
-        });
-
-
-        homeBtn = (TextView) findViewById(R.id.homeBtn);
-        homeBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Fragment homeFragment = new HomeFragment();
-                changeFragment(homeFragment);
-            }
-        });
-    }
-
-    private void getPermission() {
-        int permission = ContextCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_SMS);
-        String [] permissionList = new String[] {
-            Manifest.permission.CAMERA,
-            Manifest.permission.INTERNET,
-            Manifest.permission.RECORD_AUDIO
-        };
-
-        if (permission == PackageManager.PERMISSION_DENIED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECEIVE_SMS},
-                    100);
-        }
-        ActivityCompat.requestPermissions(this, permissionList,200);
     }
 
     @Override
@@ -206,10 +117,13 @@ public class MainMenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
 
-        emergencyIntent = new Intent(this, EmergencyService.class);
-        geoLocationIntent = new Intent(this, GeolocationService.class);;
 
-        this.getPermission();
+        presenter = new MainMenuPresenter(getApplicationContext(), this);
+        presenter.getPermission();
+        presenter.startServiceBroadcastReceiver();
+        presenter.startSMSReceiver();
+
+        initUI();
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             CharSequence name = "Lazyboy Notification Channel";
@@ -237,8 +151,6 @@ public class MainMenuActivity extends AppCompatActivity {
         EmergencyBroadcast emergencyBroadcast = new EmergencyBroadcast();
         registerReceiver(emergencyBroadcast, filter);
 
-        // Initialize Control Buttons
-        initButtonsUI();
     }
 
 
@@ -264,10 +176,6 @@ public class MainMenuActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-
-        if (EmergencyService.isRunning())
-            stopService(new Intent(this, EmergencyService.class));
-        if (GeolocationService.isRunning())
-            stopService(new Intent(this, GeolocationService.class));
+        presenter.onDestroy();
     }
 }
---
+++ app/src/main/java/com/example/guardiancamera_wifi/presentation/views/app/MainMenuPresenter.java
@@ -0,0 +1,158 @@
+package com.example.guardiancamera_wifi.presentation.views.app;
+
+import android.Manifest;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.example.guardiancamera_wifi.MyApplication;
+import com.example.guardiancamera_wifi.domain.broadcasts.EmergencyBroadcast;
+import com.example.guardiancamera_wifi.domain.services.EmergencyService;
+import com.example.guardiancamera_wifi.domain.services.GeolocationService;
+
+public class MainMenuPresenter {
+    MainMenuActivity activity;
+    Context applicationContext;
+    Intent emergencyIntent;
+    Intent geoLocationIntent;
+
+    BroadcastReceiver serviceMsgReceiver;
+    EmergencyBroadcast emergencyBroadcast;
+
+    MainMenuPresenter(Context context, MainMenuActivity activity) {
+        this.activity = activity;
+        this.applicationContext = context;
+        this.emergencyIntent = new Intent(activity, EmergencyService.class);
+        this.geoLocationIntent = new Intent(activity, GeolocationService.class);
+    }
+
+    public void getPermission() {
+        int permission = ContextCompat.checkSelfPermission(applicationContext, Manifest.permission.RECEIVE_SMS);
+        String[] permissionList = new String[]{
+                Manifest.permission.CAMERA,
+                Manifest.permission.INTERNET,
+                Manifest.permission.RECORD_AUDIO
+        };
+
+        if (permission == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.RECEIVE_SMS},
+                    100);
+        }
+        ActivityCompat.requestPermissions(activity, permissionList, 200);
+    }
+
+    public void onDestroy() {
+        if (EmergencyService.isRunning())
+            activity.stopService(new Intent(activity, EmergencyService.class));
+        if (GeolocationService.isRunning())
+            activity.stopService(new Intent(activity, GeolocationService.class));
+    }
+
+    public boolean isStreaming() {
+        return EmergencyService.isRunning();
+    }
+
+    public void startStreamingService() {
+        activity.startService(emergencyIntent);
+    }
+
+    public void stopStreamingService() {
+        activity.stopService(emergencyIntent);
+    }
+
+    public void stopGeoLocationService() {
+        if (GeolocationService.isRunning())
+            activity.stopService(geoLocationIntent);
+    }
+
+    public void startGeoLocationService(String geoDestUrl) {
+        geoLocationIntent.putExtra("destUrl", geoDestUrl);
+
+        if (GeolocationService.isRunning())
+            activity.stopService(geoLocationIntent);
+        activity.startService(geoLocationIntent);
+    }
+
+
+    public void startServiceBroadcastReceiver() {
+        IntentFilter filter = new IntentFilter();
+        filter.addAction("MainMenuActivity");
+        serviceMsgReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                String action = intent.getAction();
+                Bundle extras = intent.getExtras();
+
+                assert action != null;
+                switch (action) {
+                    case "stream.start":
+                        activity.onStreamStart(MyApplication.clientStreamData);
+                        break;
+
+                    case "camera.connected":
+                        activity.onCameraConnected();
+                        break;
+
+                    case "camera.disconnected":
+                        activity.onCameraDisconnected();
+                        break;
+
+                    case "emergency.start":
+                        startGeoLocationService(intent.getStringExtra("geoDestUrl"));
+                        activity.onEmergencyStart(MyApplication.clientStreamData);
+                        break;
+
+                    case "emergency.stop":
+                        stopGeoLocationService();
+                        activity.onEmergencyStop();
+                        break;
+
+                    case "stream.stop":
+                        activity.onStreamStop();
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        };
+        activity.registerReceiver(serviceMsgReceiver, filter);
+    }
+
+    public void startSMSReceiver() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "Lazyboy Notification Channel";
+            String description = "Emergency Notification Channel";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel("LazyBoyChannel", name, importance);
+            channel.setDescription(description);
+            NotificationManager notificationManager = activity.getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        /*
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "LazyBoyChannel")
+                .setSmallIcon(R.drawable.kakaoaccount_icon)
+                .setContentTitle("Emergency Notification")
+                .setContentText("Emergency Text")
+                .setPriority(NotificationCompat.PRIORITY_MAX)
+                .setDefaults(NotificationCompat.DEFAULT_ALL);
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        notificationManager.notify(0, builder.build());
+        */
+
+        IntentFilter filter = new IntentFilter();
+        filter.addAction("android.provider.Telephony.SMS_RECEIVED");
+        emergencyBroadcast = new EmergencyBroadcast();
+        activity.registerReceiver(emergencyBroadcast, filter);
+    }
+}
