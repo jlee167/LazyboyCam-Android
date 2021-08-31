ad1a824ea718541a656278a2b63a64b075aae525
Jihoon Lee <anfwkrpdnjs179@gmail.com>
2021-09-01 04:08:42 +0900

    Refactoring: Divide functions into social and non-social parts. +Some minor cleanups.

--- app/src/main/java/com/example/guardiancamera_wifi/data/api/http/MainServerConnection.java
+++ app/src/main/java/com/example/guardiancamera_wifi/data/api/http/MainServerConnection.java
@@ -1,26 +1,14 @@
 package com.example.guardiancamera_wifi.data.api.http;
 
-import android.content.Context;
 import android.text.TextUtils;
 import android.util.Log;
 
+import com.example.guardiancamera_wifi.data.api.http.base.HttpConnection;
 import com.example.guardiancamera_wifi.data.configs.Addresses;
 import com.example.guardiancamera_wifi.data.configs.LazyWebURI;
-import com.example.guardiancamera_wifi.data.api.http.base.HttpConnection;
+import com.example.guardiancamera_wifi.domain.models.Types;
 import com.example.guardiancamera_wifi.domain.models.LazyWebPeers;
 import com.example.guardiancamera_wifi.domain.models.LazyWebUser;
-import com.example.guardiancamera_wifi.domain.libs.types.Types;
-import com.example.guardiancamera_wifi.MyApplication;
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.kakao.auth.Session;
-import com.kakao.network.ErrorResult;
-import com.kakao.usermgmt.UserManagement;
-import com.kakao.usermgmt.callback.MeV2ResponseCallback;
-import com.kakao.usermgmt.response.MeV2Response;
-import com.kakao.usermgmt.response.model.Profile;
-import com.kakao.usermgmt.response.model.UserAccount;
-import com.kakao.util.OptionalBoolean;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -32,7 +20,6 @@ import java.io.IOException;
 import java.net.CookieManager;
 import java.net.HttpCookie;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URI;
 import java.net.URL;
@@ -47,135 +34,21 @@ import java.util.concurrent.Future;
 
 public class MainServerConnection {
 
-    private Types.OAuthProvider authProvider;
 
-    /* Caller activity context & passed intent */
-    private Context appContext;
 
-    /* Login Information */
-    private GoogleSignInAccount googleAccount;
-    private UserAccount kakaoAccount;
+    private static boolean session_enabled;
+    private static String COOKIES;
+    private CookieManager cookieManager;
 
-    private BufferedOutputStream outputStream;
-    private BufferedInputStream inputStream;
+    private ExecutorService executor;
 
-    private static boolean session_enabled = false;
-    private static String COOKIES = "";
-    private CookieManager cookieManager = new CookieManager();
 
-    private ExecutorService executor = Executors.newFixedThreadPool(2);
-
-    /**
-     * Default Constructor.
-     * Get the owner's environments and copy to the local variables.
-     *
-     * @param applicationContext Current application's context
-     * @throws MalformedURLException
-     */
-    public MainServerConnection(final Context applicationContext, final Types.OAuthProvider auth)
-            throws IOException {
-        appContext = applicationContext;
-        authProvider = auth;
-        registerOAuthAccount();
+    public MainServerConnection() {
+        session_enabled = false;
+        COOKIES = "";
+        cookieManager = new CookieManager();
+        executor = Executors.newFixedThreadPool(2);
     }
-
-
-    public MainServerConnection(final Context applicationContext)
-            throws IOException {
-        appContext = applicationContext;
-    }
-
-
-    public Types.OAuthProvider getAuthProvider() {
-        return this.authProvider;
-    }
-
-    public void setAuthProvider(Types.OAuthProvider authProvider) {
-        this.authProvider = authProvider;
-    }
-
-
-    public String getKakaoAccessToken() {
-        return Session.getCurrentSession().getTokenInfo().getAccessToken();
-    }
-
-    public String getGoogleAccessToken() {
-        return googleAccount.getIdToken();
-    }
-
-
-    /**
-     * Get user's social login account
-     * Store account instance to either 'googleAccount' or 'kakaoAccount' variable
-     *
-     * @return True if signed in with Kakao or Google
-     */
-    public void registerOAuthAccount() {
-
-        switch (this.authProvider) {
-            case AUTHENTICATOR_GOOGLE: {
-                googleAccount = GoogleSignIn.getLastSignedInAccount(appContext);
-                break;
-            }
-
-            case AUTHENTICATOR_KAKAO: {
-                UserManagement.getInstance().me(new MeV2ResponseCallback() {
-                    @Override
-                    public void onSessionClosed(ErrorResult errorResult) {
-                        Log.e("KAKAO_API", "세션이 닫혀 있음: " + errorResult);
-                    }
-
-                    @Override
-                    public void onFailure(ErrorResult errorResult) {
-                        Log.e("KAKAO_API", "사용자 정보 요청 실패: " + errorResult);
-                    }
-
-                    @Override
-                    public void onSuccess(MeV2Response result) {
-                        Log.i("KAKAO_API", "사용자 아이디: " + result.getId());
-
-                        kakaoAccount = result.getKakaoAccount();
-                        if (kakaoAccount != null) {
-                            // 이메일
-                            String email = kakaoAccount.getEmail();
-
-                            if (email != null) {
-                                Log.i("KAKAO_API", "email: " + email);
-
-                            } else if (kakaoAccount.emailNeedsAgreement() == OptionalBoolean.TRUE) {
-                                // 동의 요청 후 이메일 획득 가능
-                                // 단, 선택 동의로 설정되어 있다면 서비스 이용 시나리오 상에서 반드시 필요한 경우에만 요청해야 합니다.
-
-                            } else {
-                                // 이메일 획득 불가
-                            }
-
-                            // 프로필
-                            Profile profile = kakaoAccount.getProfile();
-
-                            if (profile != null) {
-                                Log.d("KAKAO_API", "nickname: " + profile.getNickname());
-                                Log.d("KAKAO_API", "profile image: " + profile.getProfileImageUrl());
-                                Log.d("KAKAO_API", "thumbnail image: " + profile.getThumbnailImageUrl());
-
-                            } else if (kakaoAccount.profileNeedsAgreement() == OptionalBoolean.TRUE) {
-                                // 동의 요청 후 프로필 정보 획득 가능
-
-                            } else {
-                                // 프로필 획득 불가
-                            }
-                        }
-                    }
-                });
-
-                break;
-            }
-
-            default:
-                return;
-        }
-    }
-
 
     public void pingServer() throws InterruptedException {
         Thread pingThread = new Thread(new Runnable() {
@@ -187,7 +60,7 @@ public class MainServerConnection {
             public void run() {
                 try {
                     sendHttpRequest(url, token_json, method);
-                } catch (IOException | JSONException e) {
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
@@ -197,90 +70,67 @@ public class MainServerConnection {
     }
 
 
-    public JSONObject getCredential(String username, String password)
+    public JSONObject getNonSocialCredential(String username, String password)
             throws JSONException {
         JSONObject authCredential = new JSONObject();
-
-        switch (this.authProvider) {
-            case AUTHENTICATOR_GOOGLE: {
-                authCredential.put("accessToken", getGoogleAccessToken());
-                break;
-            }
-
-            case AUTHENTICATOR_KAKAO: {
-                authCredential.put("accessToken", getKakaoAccessToken());
-                break;
-            }
-
-            default: {
-                authCredential.put("username", username);
-                authCredential.put("password", password);
-                break;
-            }
-        }
-
+        authCredential.put("username", username);
+        authCredential.put("password", password);
         return authCredential;
     }
 
+    public JSONObject getOAuthCredential(String accessToken) throws JSONException {
+        JSONObject authCredential = new JSONObject();
+        authCredential.put("accessToken", accessToken);
+        return authCredential;
+    }
 
-    /**
-     * @param uri
-     * @param tokens_json
-     * @param method
-     * @return
-     * @throws IOException
-     * @throws JSONException
-     */
-    public String sendHttpRequest(String uri, JSONObject tokens_json, String method)
-            throws IOException, JSONException {
-        URL authServerUrl;
+    public String sendHttpRequest(String uri, JSONObject body, String httpMethod) throws IOException {
+
+        BufferedOutputStream outputStream;
+        BufferedInputStream inputStream;
+
+        String urlString = Addresses.PREFIX_HTTP + Addresses.MAIN_SERVER_URL + uri;
+        URL authServerUrl = new URL(urlString);
         HttpURLConnection httpConn;
-
-        authServerUrl = new URL(Addresses.PREFIX_HTTP
-                + Addresses.MAIN_SERVER_URL
-                + uri);
-
-        boolean outputEnabled = method.equals(HttpConnection.POST) || method.equals(HttpConnection.PUT);
+        boolean outputEnabled = httpMethod.equals(HttpConnection.POST) || httpMethod.equals(HttpConnection.PUT);
 
         try {
             httpConn = (HttpURLConnection) authServerUrl.openConnection();
         } catch (IOException e) {
             e.printStackTrace();
-            return null;
+            throw e;
         }
 
-        if (session_enabled)
+        if (session_enabled) {
             httpConn.setRequestProperty("Cookie", COOKIES);
-
+        }
         httpConn.setRequestProperty("Content-Type", "application/json; utf-8");
         httpConn.setRequestProperty("Accept", "application/json");
         try {
-            httpConn.setRequestMethod(method);
+            httpConn.setRequestMethod(httpMethod);
         } catch (ProtocolException e) {
             e.printStackTrace();
         }
 
-        if (outputEnabled) {
+        if (outputEnabled)
             httpConn.setDoOutput(true);
-        }
-
         httpConn.setDoInput(true);
         httpConn.connect();
 
-
         if (outputEnabled) {
             try {
-                /* Http output stream */
                 outputStream = new BufferedOutputStream(httpConn.getOutputStream());
             } catch (IOException e) {
                 e.printStackTrace();
+                throw e;
             }
-
-            //streamToAuthServer.write(tokens_json.toString().getBytes(StandardCharsets.UTF_8));
-            outputStream.write(tokens_json.toString().getBytes());
+            outputStream.write(body.toString().getBytes());
             outputStream.flush();
             outputStream.close();
+            //streamToAuthServer.write(body.toString().getBytes(StandardCharsets.UTF_8));
         }
+
+
         int responseCode = httpConn.getResponseCode();
         String msg = httpConn.getResponseMessage();
         inputStream = new BufferedInputStream(httpConn.getInputStream());
@@ -293,10 +143,8 @@ public class MainServerConnection {
             List<String> cookie = header.get("Set-Cookie");
             for (int i = 0; i < cookie.size(); i++) {
                 cookieManager.getCookieStore().add(
-                        URI.create(Addresses.PREFIX_HTTP
-                                + Addresses.MAIN_SERVER_URL),
-                        HttpCookie.parse(cookie.get(i)).get(0)
-                );
+                        URI.create(Addresses.PREFIX_HTTP + Addresses.MAIN_SERVER_URL),
+                        HttpCookie.parse(cookie.get(i)).get(0));
             }
             session_enabled = true;
             COOKIES = TextUtils.join(
@@ -310,6 +158,44 @@ public class MainServerConnection {
         return new String(response);
     }
 
+    public String getAuthUrl(Types.OAuthProvider authProvider) {
+        switch (authProvider) {
+            case AUTHENTICATOR_KAKAO:
+                return LazyWebURI.URI_LOGIN() + LazyWebURI.URI_KAKAO();
+
+            case AUTHENTICATOR_GOOGLE:
+                return LazyWebURI.URI_LOGIN() + LazyWebURI.URI_GOOGLE();
+
+            default: {
+                return LazyWebURI.URI_LOGIN();
+            }
+        }
+    }
+
+    public JSONObject oAuthLogin(String accessToken, Types.OAuthProvider authProvider)
+            throws ExecutionException, InterruptedException {
+
+        Callable<JSONObject> task = new Callable<JSONObject>() {
+            private String mAccessToken = accessToken;
+
+            @Override
+            public JSONObject call() throws Exception {
+                try {
+                    String uri = getAuthUrl(authProvider);
+                    JSONObject credential = getOAuthCredential(mAccessToken);
+                    String result = sendHttpRequest(uri, credential, HttpConnection.POST);
+                    return new JSONObject(result);
+                } catch (IOException | JSONException e) {
+                    e.printStackTrace();
+                    throw e;
+                }
+            }
+        };
+
+        Future<JSONObject> future = executor.submit(task);
+        return future.get();
+    }
+
 
     /**
      * Sign in to lazyweb
@@ -318,56 +204,22 @@ public class MainServerConnection {
      * @param password
      * @return Success(True) or Fail(False)
      */
-    public Boolean login(final String username, final String password)
+    public JSONObject nonSocialLogin(final String username, final String password)
             throws ExecutionException, InterruptedException {
 
-        Callable<Boolean> task = new Callable<Boolean>() {
-
-            private Types.OAuthProvider mAuthProvider = authProvider;
-            private String mUsername = username;
-            private String mPassword = password;
-            private String uri;
-            private JSONObject result;
-
-            /**
-             * Computes a result, or throws an exception if unable to do so.
-             *
-             * @return computed result
-             * @throws Exception if unable to compute a result
-             */
-            @Override
-            public Boolean call() throws Exception {
-                switch (this.mAuthProvider) {
-                    case AUTHENTICATOR_KAKAO: {
-                        uri = LazyWebURI.URI_LOGIN() + LazyWebURI.URI_KAKAO();
-                        break;
-                    }
-
-                    case AUTHENTICATOR_GOOGLE: {
-                        uri = LazyWebURI.URI_LOGIN() + LazyWebURI.URI_GOOGLE();
-                        break;
-                    }
-
-                    default: {
-                        uri = LazyWebURI.URI_LOGIN();
-                    }
-                }
-
-                try {
-                    result = new JSONObject(sendHttpRequest(
-                            uri,
-                            getCredential(mUsername, mPassword),
-                            HttpConnection.POST
-                    ));
-                } catch (IOException | JSONException e) {
-                    e.printStackTrace();
-                }
-
-                return true;
+        Callable<JSONObject> task = () -> {
+            try {
+                String uri = getAuthUrl(Types.OAuthProvider.AUTHENTICATOR_NONSOCIAL);
+                JSONObject credential = getNonSocialCredential(username, password);
+                String result = sendHttpRequest(uri, credential, HttpConnection.POST);
+                return new JSONObject(result);
+            } catch (IOException | JSONException e) {
+                e.printStackTrace();
+                throw e;
             }
         };
 
-        Future<Boolean> future = executor.submit(task);
+        Future<JSONObject> future = executor.submit(task);
         return future.get();
     }
 
