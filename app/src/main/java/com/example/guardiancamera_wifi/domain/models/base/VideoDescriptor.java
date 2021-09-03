2344e0061a3b68aafb374d7415ea016fa936a90e
Jihoon Lee <anfwkrpdnjs179@gmail.com>
2021-09-03 19:25:13 +0900

    Feat: Created video format descriptor class (VideoDescriptor).

---
+++ app/src/main/java/com/example/guardiancamera_wifi/domain/models/base/VideoDescriptor.java
@@ -0,0 +1,44 @@
+package com.example.guardiancamera_wifi.domain.models.base;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import androidx.preference.PreferenceManager;
+
+
+public class VideoDescriptor {
+
+    public enum Format {
+        MJPEG,
+        RTMP,
+        HLS,
+        RGB565
+    }
+
+    public enum Size{
+        QCIF,
+        VGA,
+        SVGA,
+        HD,
+        FULLHD
+    }
+
+    private Format format;
+    private Size size;
+
+    public void setFormat(Format format) {
+        this.format = format;
+    }
+
+    public void setSize(Size size) {
+        this.size = size;
+    }
+
+    public Format getFormat() {
+        return format;
+    }
+
+    public Size getSize() {
+        return size;
+    }
+}
