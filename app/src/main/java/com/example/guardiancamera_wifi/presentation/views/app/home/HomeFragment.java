c4f8a1ccec82a335f77d50127111823f62f80520
jlee167 <anfwkrpdnjs179@gmail.com>
2021-10-18 19:30:12 +0900

    -Refactoring: Move BroadcastReceiver from activity to presenter

--- app/src/main/java/com/example/guardiancamera_wifi/presentation/views/app/home/HomeFragment.java
+++ app/src/main/java/com/example/guardiancamera_wifi/presentation/views/app/home/HomeFragment.java
@@ -1,7 +1,10 @@
 package com.example.guardiancamera_wifi.presentation.views.app.home;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.content.Intent;
+import android.content.res.AssetManager;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,71 +16,123 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.guardiancamera_wifi.Env;
 import com.example.guardiancamera_wifi.MyApplication;
 import com.example.guardiancamera_wifi.R;
-import com.example.guardiancamera_wifi.domain.broadcasts.ServiceMsgBroadcast;
+import com.example.guardiancamera_wifi.domain.services.EmergencyService;
+import com.example.guardiancamera_wifi.domain.services.exceptions.InEmergencyException;
+import com.example.guardiancamera_wifi.domain.services.exceptions.TimeoutException;
 import com.squareup.picasso.Picasso;
+
+import java.io.InputStream;
 
 
 public class HomeFragment extends Fragment {
 
     Activity activity;
     HomePresenter presenter;
-    ServiceMsgBroadcast serviceMsgBroadcast;
 
+    TextView streamingServiceBtn;
     TextView userStatusView;
     TextView cameraStatusView;
     TextView streamStatusView;
+
+    int defaultTextColor;
+
+
+    public void updateStreamingServiceUI() {
+        if (EmergencyService.isServiceRunning()) {
+            streamingServiceBtn.setText(R.string.MENU_STOP_CAPTURE);
+        } else {
+            streamingServiceBtn.setText(R.string.MENU_START_CAPTURE);
+        }
+    }
+
+    public void initUI() {
+        streamingServiceBtn = activity.findViewById(R.id.captureStartBtn);
+        userStatusView = activity.findViewById(R.id.userStatusView);
+        cameraStatusView = activity.findViewById(R.id.cameraStatusView);
+        streamStatusView = activity.findViewById(R.id.streamStatusView);
+        defaultTextColor = userStatusView.getCurrentTextColor();
+
+        streamingServiceBtn.setText(R.string.MENU_START_CAPTURE);
+
+        this.onCameraDisconnected();
+        this.onStreamStop();
+        this.onEmergencyStop();
+
+        streamingServiceBtn.setOnClickListener(v -> {
+            try {
+                presenter.handleServiceBtnClick();
+                updateStreamingServiceUI();
+            } catch (InEmergencyException | TimeoutException e) {
+                //@Todo: Toast Error message
+                e.printStackTrace();
+            }
+        });
+
+        AssetManager assetManager = getResources().getAssets();
+        InputStream imgInputStream;
+        try {
+            ImageView serverImage = activity.findViewById(R.id.serverImage);
+            imgInputStream = assetManager.open("flat-icons/icons8-server-48.png");
+            serverImage.setImageBitmap(BitmapFactory.decodeStream(imgInputStream));
+            imgInputStream.close();
+
+            ImageView guardianImage = activity.findViewById(R.id.guardianImage);
+            imgInputStream = assetManager.open("flat-icons/icons8-operator-58.png");
+            guardianImage.setImageBitmap(BitmapFactory.decodeStream(imgInputStream));
+            imgInputStream.close();
+
+            ImageView protectedImage = activity.findViewById(R.id.protectedImage);
+            imgInputStream = assetManager.open("flat-icons/icons8-shield-64.png");
+            protectedImage.setImageBitmap(BitmapFactory.decodeStream(imgInputStream));
+            imgInputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 
     public HomeFragment() {
     }
 
 
-    public void handleStreamStart() {
+    public void onStreamStart() {
         streamStatusView.setText(R.string.STREAM_STATUS_ACTIVE);
+        streamStatusView.setTextColor(Color.rgb(0, 151, 136));
+        updateStreamingServiceUI();
     }
 
 
-    public void handleStreamStop() {
+    public void onStreamStop() {
         streamStatusView.setText(R.string.STREAM_STATUS_INACTIVE);
+        streamStatusView.setTextColor(this.defaultTextColor);
+        updateStreamingServiceUI();
     }
 
 
-    public void handleEmergencyStart() {
+    public void onEmergencyStart() {
         userStatusView.setText(R.string.USER_STATUS_EMERGENCY);
+        userStatusView.setTextColor(Color.RED);
     }
 
 
-    public void handleEmergencyStop() {
+    public void onEmergencyStop() {
         userStatusView.setText(R.string.USER_STATUS_FINE);
+        userStatusView.setTextColor(Color.rgb(0, 151, 136));
     }
 
 
-    public void handleCameraConnection() {
+    public void onCameraConnected() {
         cameraStatusView.setText(R.string.CAMERA_STATUS_CONNECTED);
+        cameraStatusView.setTextColor(Color.rgb(0, 151, 136));
     }
 
 
-    public void handleCameraDisconnection() {
+    public void onCameraDisconnected() {
         cameraStatusView.setText(R.string.CAMERA_STATUS_DISCONNECTED);
-    }
-
-
-    public void openLogs() {
-        activity.findViewById(R.id.generalInfoView).setVisibility(View.INVISIBLE);
-        activity.findViewById(R.id.logView).setVisibility(View.VISIBLE);
-    }
-
-
-    public void closeLogs() {
-        activity.findViewById(R.id.generalInfoView).setVisibility(View.VISIBLE);
-        activity.findViewById(R.id.logView).setVisibility(View.INVISIBLE);
-    }
-
-
-    public boolean isLogOpen() {
-        return activity.findViewById(R.id.logView).getVisibility() == View.VISIBLE;
+        cameraStatusView.setTextColor(this.defaultTextColor);
     }
 
 
@@ -93,16 +148,52 @@ public class HomeFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
     }
 
+    @SuppressLint("SetTextI18n")
     @Override
     public void onResume() {
         super.onResume();
-        userStatusView.setText(R.string.USER_STATUS_FINE);
-        cameraStatusView.setText(R.string.CAMERA_STATUS_DISCONNECTED);
-        streamStatusView.setText(R.string.STREAM_STATUS_INACTIVE);
-        ImageView profileImage = (ImageView) getActivity().findViewById(R.id.userProfileImage);
+
+        ImageView profileImage = activity.findViewById(R.id.userProfileImage);
         Picasso.get().load(MyApplication.currentUser.getProfileImageUrl()).into(profileImage);
-        TextView username = (TextView) getActivity().findViewById(R.id.username);
+        TextView username = activity.findViewById(R.id.username);
         username.setText(MyApplication.currentUser.getUsername());
+
+        TextView serverAddressView = activity.findViewById(R.id.serverAddressView);
+        serverAddressView.setText(Env.STREAMING_SERVER_IP);
+
+        TextView protectedsCountView = activity.findViewById(R.id.protectedsCountView);
+        String protectedsCount = Integer.toString(MyApplication.peers.getProtecteds().length);
+        protectedsCountView.setText("You have " + protectedsCount + " protecteds");
+
+        TextView guardiansCountView = activity.findViewById(R.id.guardiansCountView);
+        String guardiansCount = Integer.toString(MyApplication.peers.getGuardians().length);
+        guardiansCountView.setText("You have " + guardiansCount + " guardians");
+
+
+        onCameraDisconnected();
+        onStreamStop();
+        onEmergencyStop();
+
+
+        if (EmergencyService.isCamConnected()) {
+            onCameraConnected();
+        }
+
+        if (EmergencyService.isStreamRunning()) {
+            onStreamStart();
+        }
+
+        if (EmergencyService.isEmergency()) {
+            onEmergencyStart();
+        }
+
+        presenter.startServiceMessageReceiver();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        presenter.stopServiceMessageReceiver();
     }
 
     @Override
@@ -110,59 +201,12 @@ public class HomeFragment extends Fragment {
         super.onStart();
         activity = getActivity();
         assert activity != null;
-        presenter = new HomePresenter();
+        presenter = new HomePresenter(getActivity().getApplicationContext(), this);
+        initUI();
+    }
 
-        userStatusView = activity.findViewById(R.id.userStatusView);
-        cameraStatusView = activity.findViewById(R.id.cameraStatusView);
-        streamStatusView = activity.findViewById(R.id.streamStatusView);
-
-        HomeFragment self = this;
-        serviceMsgBroadcast = new ServiceMsgBroadcast() {
-            @Override
-            public void onStreamStart() {
-                presenter.handleStreamStart();
-                self.handleStreamStart();
-            }
-
-            @Override
-            public void onStreamStop() {
-                presenter.handleStreamStop();
-                self.handleStreamStop();
-            }
-
-            @Override
-            public void onEmergencyStart(Intent intent) {
-                presenter.handleEmergencyStart();
-                self.handleEmergencyStart();
-            }
-
-            @Override
-            public void onEmergencyStop() {
-                presenter.handleEmergencyStop();
-                self.handleEmergencyStop();
-            }
-
-            @Override
-            public void onCameraConnected() {
-                presenter.handleCameraConnection();
-                self.handleCameraConnection();
-            }
-
-            @Override
-            public void onCameraDisconnected() {
-                presenter.handleCameraDisconnection();
-                self.handleCameraDisconnection();
-            }
-        };
-
-        activity.findViewById(R.id.logToggleBtn).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (isLogOpen())
-                    closeLogs();
-                else
-                    openLogs();
-            }
-        });
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
     }
 }
--- app/src/main/java/com/example/guardiancamera_wifi/presentation/views/app/home/HomePresenter.java
+++ app/src/main/java/com/example/guardiancamera_wifi/presentation/views/app/home/HomePresenter.java
@@ -1,33 +1,86 @@
 package com.example.guardiancamera_wifi.presentation.views.app.home;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+
+import com.example.guardiancamera_wifi.domain.broadcasts.ServiceMsgBroadcast;
+import com.example.guardiancamera_wifi.domain.models.EmergencyMessages;
+import com.example.guardiancamera_wifi.domain.services.EmergencyService;
+import com.example.guardiancamera_wifi.domain.services.exceptions.InEmergencyException;
+import com.example.guardiancamera_wifi.domain.services.exceptions.TimeoutException;
+
 public class HomePresenter {
 
-    HomePresenter() {
+    Context applicationContext;
+    HomeFragment fragment;
+    Intent emergencyIntent;
 
+    BroadcastReceiver serviceMsgReceiver;
+
+
+    HomePresenter(Context context, HomeFragment fragment) {
+        this.fragment = fragment;
+        this.applicationContext = context;
+        this.emergencyIntent = new Intent(fragment.getActivity(), EmergencyService.class);
     }
 
-    public void handleCameraConnection() {
-
+    public void handleServiceBtnClick() throws InEmergencyException, TimeoutException {
+        if (!EmergencyService.isServiceRunning()) {
+            applicationContext.startService(emergencyIntent);
+        } else {
+            if (!EmergencyService.isEmergency())
+                applicationContext.stopService(emergencyIntent);
+            else
+                throw new InEmergencyException();
+        }
     }
 
-    public void handleCameraDisconnection() {
+    public void startServiceMessageReceiver() {
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(EmergencyMessages.STREAM_READY);
+        filter.addAction(EmergencyMessages.STREAM_STOPPED);
+        filter.addAction(EmergencyMessages.CAMERA_CONNECTED);
+        filter.addAction(EmergencyMessages.CAMERA_DISCONNECTED);
+        filter.addAction(EmergencyMessages.EMERGENCY_STARTED);
+        filter.addAction(EmergencyMessages.EMERGENCY_STOPPED);
 
+        serviceMsgReceiver = new ServiceMsgBroadcast() {
+            @Override
+            public void onStreamStart() {
+                fragment.onStreamStart();
+            }
+
+            @Override
+            public void onStreamStop() {
+                fragment.onStreamStop();
+            }
+
+            @Override
+            public void onEmergencyStart(Intent intent) {
+                fragment.onEmergencyStart();
+            }
+
+            @Override
+            public void onEmergencyStop() {
+                fragment.onEmergencyStop();
+            }
+
+            @Override
+            public void onCameraConnected() {
+                fragment.onCameraConnected();
+            }
+
+            @Override
+            public void onCameraDisconnected() {
+                fragment.onCameraDisconnected();
+            }
+        };
+        fragment.requireActivity().registerReceiver(serviceMsgReceiver, filter);
     }
 
-    public void handleStreamStart() {
-
+    public void stopServiceMessageReceiver() {
+        fragment.requireActivity().unregisterReceiver(serviceMsgReceiver);
     }
-
-    public void handleStreamStop() {
-
-    }
-
-    public void handleEmergencyStart() {
-
-    }
-
-    public void handleEmergencyStop() {
-
-    }
-
 }
