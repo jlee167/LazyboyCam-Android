66defa5642afa1c260b5a3c6743658b33366dc43
jlee167 <anfwkrpdnjs179@gmail.com>
2022-04-26 17:06:16 +0900

    FIX: Make RepositoryImpl use DataSource class for data fetching

--- app/src/main/java/com/example/guardiancamera_wifi/data/repository/user/UserRepositoryImpl.java
+++ app/src/main/java/com/example/guardiancamera_wifi/data/repository/user/UserRepositoryImpl.java
@@ -10,24 +10,23 @@ import com.example.guardiancamera_wifi.domain.model.User;
 import com.example.guardiancamera_wifi.domain.repository.user.UserRepository;
 
 import org.json.JSONException;
-import org.json.JSONObject;
-
 import java.util.concurrent.ExecutionException;
+
+
+
 
 public class UserRepositoryImpl implements UserRepository {
 
-    MainServer mainServer;
     UserDataSource userDataSource;
 
     public UserRepositoryImpl() {
-        mainServer = new MainServer();
-        //userDataSource = new UserDataSource(new MainServer());
+        userDataSource = new UserDataSource(new MainServer());
     }
 
     @Override
     public boolean nonSocialLogin(String username, String password)
             throws ExecutionException, InterruptedException, AuthFailed {
-        HttpResponse response = mainServer.nonSocialLogin(username, password);
+        HttpResponse response = userDataSource.nonSocialLogin(username, password);
         if (response.getCode() != 200) {
             throw new AuthFailed();
         } else {
@@ -38,7 +37,7 @@ public class UserRepositoryImpl implements UserRepository {
     @Override
     public boolean oAuthLogin(String accessToken, Types.OAuthProvider authProvider)
             throws ExecutionException, InterruptedException, AuthFailed {
-        HttpResponse response = mainServer.oAuthLogin(accessToken, authProvider);
+        HttpResponse response = userDataSource.oAuthLogin(accessToken, authProvider);
         if (response.getCode() != 200) {
             throw new AuthFailed();
         } else {
@@ -48,14 +47,14 @@ public class UserRepositoryImpl implements UserRepository {
 
     @Override
     public User getClientProfile() throws ExecutionException, InterruptedException, JSONException {
-        return new User(mainServer.getClientProfile());
+        return new User(userDataSource.getClientProfile());
     }
 
     public Peers getPeers() throws InterruptedException, ExecutionException, JSONException {
-        return mainServer.getPeers();
+        return userDataSource.getPeers();
     }
 
     public String getMyJWT() throws ExecutionException, InterruptedException {
-        return mainServer.getMyJWT();
+        return userDataSource.getMyJWT();
     }
 }
