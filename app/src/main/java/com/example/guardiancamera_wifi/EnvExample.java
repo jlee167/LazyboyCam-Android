60decc120c0f3fede0bb816638eb04b5ede66c67
Jihoon Lee <anfwkrpdnjs179@gmail.com>
2022-06-06 16:19:21 +0900

    FEAT: Dummy .Env file for programmers to modify and use. Must change file name to Env.java before compiling

---
+++ app/src/main/java/com/example/guardiancamera_wifi/EnvExample.java
@@ -0,0 +1,13 @@
+package com.example.guardiancamera_wifi;
+
+public class EnvExample {
+    public static String GOOGLE_CLIENT_ID = "YOUR GOOGLE CLIENT ID";
+    public final static String MAIN_SERVER_URL = "www.example.com:80";
+    public final static String MAIN_SERVER_IP = "0.0.0.0:80";
+    public final static String STREAMING_SERVER_IP = "www.example.com:3001";
+    public final static String HOTSPOT_HOST_IP = "192.168.43.1";
+    public final static String HOTSPOT_DEVICE_IP = "192.168.43.74";
+
+    public final static int MAX_THREADS_MAIN_SERVER = 2;
+    public final static int MAX_THRAEDS_STREAMING_SERVER = 4;
+}
